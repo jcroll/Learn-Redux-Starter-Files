08be5dfddc36ff5ba349abf0a0cca55d8dc20931
Jarrett Croll <jarrett.croll@gmail.com>
2016-07-01 15:52:01 -0400

    lesson 2

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,33 +1,53 @@
 var path = require('path');
 var webpack = require('webpack');
 
+const PATHS = {
+  client: path.join(__dirname, 'client'),
+  static: path.join(__dirname, 'static')
+};
+
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './client/reduxstagram'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
+  entry: PATHS.client,
+  output: {
+    path:PATHS.static,
+    filename: 'bundle.js'
+  },
+  watchOptions: {
+    poll: true
+  },
+  devServer: {
+    contentBase: PATHS.static,
+    stats: 'errors-only',
+    colors: true,
+    progress: true,
+    host: '0.0.0.0',
+    inline: true,
+    hot: true,
+    historyApiFallback: true
+  },
+  devtool: 'eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [
     // js
     {
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'client')
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        cacheDirectory: true,
+        presets: ['react', 'es2015']
+      },
+      include: PATHS.client
     },
     // CSS
     { 
       test: /\.styl$/, 
-      include: path.join(__dirname, 'client'),
+      include: PATHS.client,
       loader: 'style-loader!css-loader!stylus-loader'
     }
     ]
