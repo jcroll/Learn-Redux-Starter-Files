70b750e2fe55448ac0209617271255747e33b04b
Jarrett Croll <jarrett.croll@gmail.com>
2016-07-03 21:28:05 -0400

    lesson 12

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,15 @@
 function posts(state = [], action) {
-  console.log('the post will change');
-  console.log(state, action);
-  return state;
+  const i = action.index;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
